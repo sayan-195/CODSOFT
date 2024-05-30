72cfb8116efb5f9daa90fb894f95919f957523a0
sayan-195 <sayanghosh0195@gmail.com>
2024-05-30 14:25:50 +0530

    Adding new feature

--- Number_guessing_game.java
+++ Number_guessing_game.java
@@ -4,6 +4,7 @@ class Game {
     public int number;
     public int inpNumber;
     public int numberOfGuess=0;
+    public int score=10;
 
     public int getnumberOfGuess() {
         return numberOfGuess;
@@ -20,6 +21,7 @@ class Game {
 
     void takeUserInput() {
         System.out.println("Welcome to the Game !");
+        System.out.println("Attempts "+(10-numberOfGuess)+" left");
         System.out.print("Guess The Number between 1 to 100 : ");
         Scanner sc = new Scanner(System.in);
         inpNumber = sc.nextInt();
@@ -28,9 +30,17 @@ class Game {
 
     boolean isCorrect() {
         numberOfGuess++;
+        score--;
+        while(numberOfGuess>9){
+            System.out.println("Your limit is Over !");
+            System.exit(0);
+
+        }
+        //System.out.println("Attempts "+(10-numberOfGuess)+" left");
         if (inpNumber == number) {
             System.out.println("Congratulations ! You Guessed the Right number");
             System.out.println("You Guessed the number in "+numberOfGuess+" attempts");
+            System.out.println("Your Score is : "+score+" out of 10");
             return true;
         } else if (inpNumber < number) {
             System.out.println("Too Low....");
@@ -48,11 +58,14 @@ public class Number_guessing_game {
     public static void main(String[] args) {
         Game g = new Game();
         boolean b=false;
+        
         while(!b){
         g.takeUserInput();
-        b = g.isCorrect();
+        b=g.isCorrect();
         System.out.println(b);
         }
+        
+
     }
 
 }
